c7fab17dd1c6a983c3b4625c49c687aaf9b589fa
Mike Tkachuk <mtkachuk@tvo.org>
2024-09-02 17:34:04 -0400

    added aunonymous authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
 // App.jsx
-import { useState } from 'react';
-import { AppShell, Button, Group, Container } from '@mantine/core';
+import { useEffect, useState } from "react";
+import { AppShell, Button, Group, Container } from "@mantine/core";
+// Auth
+import { auth } from "./firebaseConfig"; // Import the auth instance
+import { signInAnonymously, onAuthStateChanged } from "firebase/auth";
+
+// Components
 import Gallery from "./subcomponents/Gallery";
 import Uploader from "./subcomponents/Uploader";
 
 function App() {
-  const [activeComponent, setActiveComponent] = useState('gallery');
+  const [activeComponent, setActiveComponent] = useState("gallery");
+
+  useEffect(() => {
+    // Check if the user is already signed in
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in
+        console.log("User ID:", user.uid);
+      } else {
+        // User is not signed in, sign them in anonymously
+        signInAnonymously(auth)
+          .then(() => {
+            console.log("Signed in anonymously");
+          })
+          .catch((error) => {
+            console.error("Anonymous sign-in failed:", error);
+          });
+      }
+    });
+  }, []);
 
   return (
     <AppShell>
       <Container>
         <Group justify="center" mt="md" mb="xl">
-          <Button onClick={() => setActiveComponent('gallery')}>Gallery</Button>
-          <Button onClick={() => setActiveComponent('upload')}>Upload</Button>
+          <Button onClick={() => setActiveComponent("gallery")}>Gallery</Button>
+          <Button onClick={() => setActiveComponent("upload")}>Upload</Button>
         </Group>
-        
-        {activeComponent === 'gallery' ? <Gallery /> : <Uploader />}
+
+        {activeComponent === "gallery" ? <Gallery /> : <Uploader />}
       </Container>
     </AppShell>
   );
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,9 @@
 // firebaseConfig.js
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
 
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA1-OeRt37mba0N9CkVenELWVdEYu5aZ6Y",
   authDomain: "edward--nicole-wedding.firebaseapp.com",
@@ -16,9 +16,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Firestore and Analytics
-const db = getFirestore(app);
-export const analytics = getAnalytics(app);
 
-export { db };
+// Initialize Firebase services
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
 export default app;
